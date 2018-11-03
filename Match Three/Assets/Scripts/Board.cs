3970292a67909ebabafec6f166e130292d4ae008
DeXoteric <marlev@poczta.pl>
2018-11-03 20:22:15 +0100

    2_5

---
+++ Match Three/Assets/Scripts/Board.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+
+public class Board : MonoBehaviour
+{
+    [SerializeField] int width;
+    [SerializeField] int height;
+
+    [SerializeField] GameObject tilePrefab;
+
+    Tile[,] allTiles;
+
+    private void Start()
+    {
+        allTiles = new Tile[width, height];
+
+        SetupTiles();
+    }
+
+    void SetupTiles()
+    {
+        for (int x = 0; x < width; x++)
+        {
+            for (int y = 0; y < height; y++)
+            {
+                GameObject tile = Instantiate(tilePrefab, new Vector3(x, y, 0), Quaternion.identity) as GameObject;
+                tile.name = "Tile (" + x + "," + y + ")";
+
+                allTiles[x, y] = tile.GetComponent<Tile>();
+
+                tile.transform.parent = transform;
+            }
+        }
+    }
+}
